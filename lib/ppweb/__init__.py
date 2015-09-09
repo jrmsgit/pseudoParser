f605025466723d81c8d7ad35706d2b755fbe514e
Jeremías Casteglione <git@jrms.com.ar>
2015-09-08 23:28:12 -0300

    basic exec page and set ppweb cookie

--- lib/ppweb/__init__.py
+++ lib/ppweb/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import os.path
-from bottle import Bottle, template, static_file, request
+from bottle import Bottle, template, static_file, request, response
 
 _DEBUG = True
 _CODEDIR = os.path.dirname(__file__)
@@ -37,28 +37,45 @@ class ppWebApp(Bottle):
     Req = None
     Log = None
     Msg = None
+    Resp = None
 
     def __init__(self):
         self.Log = wappLogger()
         self.Log.dbg('init')
         self.Req = request
-        self.Template('index.html')
         self.Msg = wappMessages()
+        self.Resp = response
         return super(ppWebApp, self).__init__()
 
-    def Template(self, tname):
+    def _setTemplate(self, tname):
         self._tmpl = os.path.join('ppweb', 'templates', tname)
 
     def Run(self):
+        self.Log.dbg('Run')
         return super(ppWebApp, self).run(host='jrmsdev.local', debug=_DEBUG)
 
+    def Start(self, template='index.html'):
+        self.Log.dbg('Start')
+
+        # template
+        self._setTemplate(template)
+
+        # ppweb cookie
+        self.Log.dbg('cookies:', self.Req.cookies.keys())
+        cookie = self.Req.get_cookie('ppweb')
+        self.Log.dbg('ppweb cookie:', cookie)
+        if not cookie:
+            self.Resp.set_cookie('ppweb', 'LALALA')
+
     def Render(self):
+        self.Log.dbg('Render')
         tmplArgs = {
             'wappMessages': self.Msg.getAll(),
         }
         return template("% include('{}')".format(self._tmpl), **tmplArgs)
 
     def SendFile(self, filename):
+        self.Log.dbg('SendFile')
         return static_file(filename, root=os.path.join(_CODEDIR, 'static'))
 
 
--- lib/ppweb/views/index.py
+++ lib/ppweb/views/index.py
@@ -1,15 +1,24 @@
+import sys
 from .. import wapp
+
 
 @wapp.get('/')
 def index():
+    wapp.Start()
     return wapp.Render()
 
 
 @wapp.get('/static/<filename:path>')
 def sendFile(filename):
+    wapp.Start()
     return wapp.SendFile(filename)
 
 
 @wapp.post('/exec/')
 def codeExec():
+    wapp.Start(template='exec.html')
+    ppCode = wapp.Req.forms.get('ppCode', '')
+    wapp.Log.dbg("ppCode:", ppCode)
+    if ppCode == '':
+        wapp.Msg.error('no hay código para ejecutar')
     return wapp.Render()
